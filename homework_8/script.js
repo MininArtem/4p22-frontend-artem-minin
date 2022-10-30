92afe8795d7200a65f51faea18f39501ea6b7db6
Artem Minin <timeap@yandex.ru>
2022-10-30 23:33:45 +0300

    finished homework_8

--- homework_8/script.js
+++ homework_8/script.js
@@ -1,22 +1,55 @@
 'use strict'
 
-let firstNumber = document.getElementById('firstNumber');
-let secondNumber = document.getElementById('secondNumber');
-let operator = document.getElementById('operator');
-const btn = document.getElementById('submitButton');
-let result = document.getElementById('result');
-result.readOnly = true;
+const button = document.getElementById('submitButton');
+let resultValue = document.getElementById('result');
 
-btn.addEventListener('click', (event) => {
+button.addEventListener('click', (event) => {
   event.preventDefault();
-  switch (operator.value) {
-    case '*':
-      result.value = firstNumber.value * secondNumber.value;
+  let firstNumber = document.getElementById('firstNumber').value;
+  let secondNumber = document.getElementById('secondNumber').value;
+  let operator = document.getElementById('operator').value;
+  let result;
+  const firstValue = Number(firstNumber);
+  const secondValue = Number(secondNumber);
+
+  switch (operator.trim()) {
+    case '':
+      result = 'Не введен знак';
       break;
+    case '*':
+      result = firstValue * secondValue;
+      break;
+    case '/':
+      if(secondValue === 0) {
+        result = `Операция некорректна! <br> Нельзя делить на ноль`;
+        break;
+      } else {
+        result = firstValue / secondValue;
+        break;
+      }
+    case '+':
+      result = firstValue + secondValue;
+      break;
+    case '-':
+      result = firstValue - secondValue;;
+      break;
+    default:
+      result = 'Программа не поддерживает такую операцию';
+      break;
+    }
+
+  if (firstNumber.trim() === '') {
+    result = 'Не указано первое число'; 
+  }
+  if(secondNumber.trim() === '') {
+    result = 'Не указано второе число';
+  }
+  if(isNaN(firstNumber) || isNaN(secondNumber)) {
+    result = 'Некорректный ввод чисел';
+  }
+  if (result === Infinity || result === -Infinity) {
+    result = 'Операция некорректна';  
+  } else {
+    resultValue.innerHTML = result;
   }
 });
-
-
-
-
-
